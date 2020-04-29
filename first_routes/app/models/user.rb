a72108bf70845137d14381c3f3437f30bb55b29d
Daniel Gorter <danieljgorter@gmail.com>
2020-04-28 23:36:22 -0400

    update favorite validation

--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -36,20 +36,27 @@ class User < ApplicationRecord
     # through: :likes,
     # source: :liked_artwork
 
+    def favorite(artwork_id)
+        return nil unless valid_favorite?(artwork_id)
+
+        self.favorite_id = artwork_id
+        true
+    end
+
 
     private
     def favorite_must_be_own_or_share
-        if favorite_id
-            result = User
-                .left_outer_joins(:artworks, :shared_artworks)
-                .where("users.id = ?", self.id)
-                .where("artworks.id = #{favorite_id} OR shared_artworks_users.id = #{favorite_id}")
-                .count
-            # result = User.joins(:artworks, :shared_artworks).where("artworks.id = 10 OR shared_artworks_users.id = 10")
-            if result == 0
-                errors[:favorite_id] << "is not owned or shared with user"
-            end
+        unless favorite_id.nil? || valid_favorite?(favorite_id)
+            errors[:favorite_id] << "is not owned or shared with user" 
         end
     end
-
-end
+    
+    def valid_favorite?(artwork_id)
+        result = User
+            .left_outer_joins(:artworks, :shared_artworks)
+            .where("users.id = ?", self.id)
+            .where("artworks.id = #{artwork_id} OR shared_artworks_users.id = #{artwork_id}")
+            .count
+        
+        result > 0
+    end
